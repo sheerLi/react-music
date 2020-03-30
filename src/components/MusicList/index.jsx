1c58a58f7f177fc1c63bd6fa15e1dac4e8c8d4ee
sheerLi <m17805850283@163.com>
2020-03-30 23:40:11 +0800

    feat: 样式调整

--- src/components/MusicList/index.jsx
+++ src/components/MusicList/index.jsx
@@ -5,19 +5,17 @@ import styles from "./style.less";
 export default ({ list }) => (
   <div className={styles.listWrap}>
     <div className={classnames(styles.listHeader, styles.listItem)}>
-      <span className={styles.listItem__name}>歌曲</span>
-      <span className={styles.listItem__singer}>歌手</span>
-      <span className={styles.listItem__time}>时常</span>
-      <span className={styles.listItem__album}>专辑</span>
+      <div className={styles.listItem__name}>歌曲</div>
+      <div className={styles.listItem__singer}>歌手</div>
+      <div className={styles.listItem__time}>时长</div>
     </div>
     <div className={styles.listContent}>
       {list.map((item, index) => (
         <div className={styles.listItem} key={item.id}>
-          <span className={styles.listItem__num}>{index + 1}</span>
-          <span className={styles.listItem__name}>{item.name}</span>
-          <span className={styles.listItem__singer}>{item.singer}</span>
-          <span className={styles.listItem__time}>{item.duration}</span>
-          <span className={styles.listItem__album}>{item.album}</span>
+          <div className={styles.listItem__num}>{index + 1}</div>
+          <div className={styles.listItem__name}>{item.name}</div>
+          <div className={styles.listItem__singer}>{item.singer}</div>
+          <div className={styles.listItem__time}>{item.duration}</div>
         </div>
       ))}
     </div>
--- src/routes/playing/index.jsx
+++ src/routes/playing/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import { Loading, Button, AlbumInfo, MusicList } from '@/components';
-import { getTopListRequest } from '@/redux/actions';
+import { Loading, Button, AlbumInfo, MusicList } from "@/components";
+import { getTopListRequest } from "@/redux/actions";
 
-import styles from './style.less';
+import styles from "./style.less";
 
-const Buttons = [{ key: 'playing', text: '正在播放', path: '/playing' }];
+const Buttons = [{ key: "playing", text: "正在播放", path: "/playing" }];
 
 class Playing extends React.Component {
   state = {};
@@ -19,43 +19,47 @@ class Playing extends React.Component {
   render() {
     const { isFetching, topList } = this.props;
     return (
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.left}>
-            <div className={styles.btnGroup}>
-              {Buttons.map(item => (
-                <Button key={item.key} className={styles.btn} text={item.text} />
-              ))}
+      <>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <div className={styles.left}>
+              <div className={styles.btnGroup}>
+                {Buttons.map(item => (
+                  <Button
+                    key={item.key}
+                    className={styles.btn}
+                    text={item.text}
+                  />
+                ))}
+              </div>
+              <div className={styles.main}>
+                <MusicList list={topList} />
+                {isFetching && <Loading />}
+              </div>
             </div>
-            <div className={styles.main}>
-              <MusicList list={topList} />
+            <div className={styles.right}>
+              <AlbumInfo />
             </div>
-
-            {isFetching && <Loading />}
-          </div>
-          <div className={styles.right}>
-            <AlbumInfo />
           </div>
         </div>
-
         {/* 遮罩层 */}
         <div className={styles.playerBg}></div>
         <div className={styles.playerMask}></div>
-      </div>
+      </>
     );
   }
 }
 
 const mapState = state => ({
   isFetching: state.app.isFetching,
-  topList: state.recommend.topList,
+  topList: state.recommend.topList
 });
 
 const mapDispatch = dispatch => {
   return {
     getTopList: payload => {
       dispatch(getTopListRequest(payload));
-    },
+    }
   };
 };
 
