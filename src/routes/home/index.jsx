55bff7b6a73b0280abc901615cc212d60772f93e
sheerLi <m17805850283@163.com>
2020-04-13 19:09:59 +0800

    feat: 首页layout

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -1,16 +1,60 @@
-import React, { Component } from 'react';
-import { Button } from "antd";
+import React, { Component } from "react";
+import { Layout, Menu, Carousel } from "antd";
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+
+import styles from "./style.less";
+
+const { Sider, Content } = Layout;
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
   render() {
     return (
-      <div>
-        <Button type="primary">和卡好卡</Button>
-      </div>
+      <Layout style={{ height: "100vh" }}>
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1">
+              <UserOutlined />
+              <span>nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <VideoCameraOutlined />
+              <span>nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <UploadOutlined />
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Content className="site-layout-background">
+            <div className={styles.carouselWrap}>
+              <Carousel>
+                {[1, 2, 3].map((item, i) => (
+                  <div className={styles.wrap}>
+                    <h3>{item}</h3>
+                  </div>
+                ))}
+              </Carousel>
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
     );
   }
 }
